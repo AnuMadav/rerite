e9eb378e8acac384cead66aa2adf433d7fc143ca
MAADHAVA_ANUSUYAA C <maadhavaanusuyaa1991@gmail.com>
2022-03-11 13:13:49 +0530

    New outlook

--- app.js
+++ app.js
@@ -6,15 +6,13 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "./static")));
-
-var today = new Date();
 var options = {
   weekday: "long",
   day: "numeric",
   month: "long",
 };
-var day = today.toLocaleDateString("en-US", options);
+
+app.use(express.static("static"));
 
 var margNames = [];
 var sarg1Names = [];
@@ -42,6 +40,11 @@ var wopinionbys = [];
 var wbooktitles = [];
 var wauthortitles = [];
 var wpublications = [];
+var urusernames = [];
+var myagreeNames = [];
+var myqueryNames = [];
+var mylistnames = [];
+var mylistargnames = [];
 
 app.set("view engine", "ejs");
 
@@ -83,7 +86,119 @@ const argsSchema = {
   bpne: String,
 };
 
+//MY ESSENTIALS
+const myessentialsSchema = {
+  myusername: {
+    type: String,
+  },
+  myenrollmentnumber: {
+    type: String,
+  },
+  myemailadress: {
+    type: String,
+  },
+  mylistname: {
+    type: String,
+  },
+  urenrollmentnumber: {
+    type: String,
+  },
+};
+
+//MY STUDYTEAM
+const mystudyteamsSchema = {
+  urenrollmentnumber: {
+    type: String,
+  },
+  urusername: {
+    type: String,
+  },
+  myenrollmentnumber: {
+    type: String,
+  },
+};
+
+//MY SAVEDARG
+const mysavedargsSchema = {
+  myenrollmentnumber: {
+    type: String,
+  },
+  mylistargname: {
+    type: String,
+  },
+  myuida: {
+    type: String,
+  },
+};
+
+//MY REFERRED
+const myreferredSchema = {
+  myenrollmentnumber: {
+    type: String,
+  },
+  uruida: {
+    type: String,
+  },
+  urenrollrefname: {
+    type: String,
+  },
+};
+
+//MY SOCIALS
+const mysocialSchema = {
+  myagreecomment: {
+    type: String,
+  },
+  myquerycomment: {
+    type: String,
+  },
+  anuida: {
+    type: String,
+  },
+  myenrollmentnumber: {
+    type: String,
+  },
+};
+
 const Arg = mongoose.model("Arg", argsSchema);
+const Myessential = mongoose.model("Myessential", myessentialsSchema);
+const Mystudyteam = mongoose.model("Mystudyteam", mystudyteamsSchema);
+const Mysavedarg = mongoose.model("Mysavedarg", mysavedargsSchema);
+const Myreferred = mongoose.model("Myreferred", myreferredSchema);
+const Mysocial = mongoose.model("Mysocial", mysocialSchema);
+
+const myessential1 = new Myessential({
+  myusername: "",
+  myenrollmentnumber: "",
+  myemailadress: "",
+  mylistname: "",
+  urenrollmentnumber: "",
+});
+
+const mystudyteam1 = new Mystudyteam({
+  myenrollmentnumber: "",
+  urusername: "",
+  urenrollmentnumber: "",
+});
+
+const mysavedarg1 = new Mysavedarg({
+  myenrollmentnumber: "",
+  mylistargname: "",
+  myuida: "",
+});
+
+const myreferred1 = new Myreferred({
+  myenrollmentnumber: "",
+  uruida: "",
+  urenrollrefname: "",
+});
+
+const mysocial1 = new Mysocial({
+  myagreecomment: "",
+  myquerycomment: "",
+  anuida: "",
+  myenrollmentnumber: "",
+});
 
 const arg1 = new Arg({
   marg: "",
@@ -110,68 +225,107 @@ const arg1 = new Arg({
   bpne: "",
 });
 
-const defaultArgs = [];
+Arg.find(
+  { ida: { $exists: true } },
+  null,
+  { sort: { _id: -1 } },
+  function (err, args) {
+    args.forEach(function (arg) {
+      var mainArgument = arg.marg;
+      var supArgument1 = arg.sarg1;
+      var supArgument2 = arg.sarg2;
+      var supArgument3 = arg.sarg3;
+      var opeddate = arg.opd;
+      var newsjournal = arg.njs;
+      var chapter = arg.id3;
+      var termtitle = arg.trm;
+      var termexplanation = arg.texp;
+      var uida = arg.ida;
+      var wnew = arg.nws;
+      var wpoint1 = arg.pnt1;
+      var wpoint2 = arg.pnt2;
+      var wpoint3 = arg.pnt3;
+      var wpoint4 = arg.pnt4;
+      var wpoint5 = arg.pnt5;
+      var wopinionon = arg.opnn;
+      var wopinion1 = arg.opn1;
+      var wopinionby = arg.opnb;
+      var wbooktitle = arg.btle;
+      var wauthortitle = arg.btar;
+      var wpublication = arg.bpne;
 
-Arg.insertMany(defaultArgs, function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Successfully saved items to DB.");
+      mainArguments.push(mainArgument);
+      supArgument1s.push(supArgument1);
+      supArgument2s.push(supArgument2);
+      supArgument3s.push(supArgument3);
+      opiniondates.push(opeddate);
+      newspaperjournals.push(newsjournal);
+      chapters.push(chapter);
+      termtitles.push(termtitle);
+      termexplanations.push(termexplanation);
+      uidas.push(uida);
+      wnewss.push(wnew);
+      wpoint1s.push(wpoint1);
+      wpoint2s.push(wpoint2);
+      wpoint3s.push(wpoint3);
+      wpoint4s.push(wpoint4);
+      wpoint5s.push(wpoint5);
+      wopinionons.push(wopinionon);
+      wopinion1s.push(wopinion1);
+      wopinionbys.push(wopinionby);
+      wbooktitles.push(wbooktitle);
+      wauthortitles.push(wauthortitle);
+      wpublications.push(wpublication);
+    });
   }
+).limit(100);
+
+Mystudyteam.find(
+  { urenrollmentnumber: { $exists: true, $ne: null } },
+  null,
+  { sort: { _id: -1 } },
+  function (err, mystudyteams) {
+    mystudyteams.forEach(function (mystudyteam) {
+      var urusername = mystudyteam.urusername;
+
+      urusernames.push(urusername);
+    });
+  }
+).limit(10);
+
+Myessential.find(
+  {
+    $and: [
+      { mylistname: { $exists: true, $ne: null } },
+      { uida: { $exists: false } },
+    ],
+  },
+
+  null,
+  { sort: { _id: -1 } },
+  function (err, studyteams) {
+    studyteams.forEach(function (studyteam) {
+      var mylistname = studyteam.mylistname;
+
+      mylistnames.push(mylistname);
+    });
+  }
+).limit(4);
+
+Mysocial.find({}, function (err, social) {
+  social.forEach(function (social) {
+    var myagreecomment = social.agreeName;
+    var myquerycomment = social.queryName;
+
+    myagreeNames.push(myagreecomment);
+    myqueryNames.push(myquerycomment);
+  });
 });
 
-Arg.find({}, null, { sort: { _id: -1 } }, function (err, args) {
-  console.log(args);
+app.get("/index", function (req, res) {
+  var today = new Date();
+  var day = today.toLocaleDateString("en-US", options);
 
-  args.forEach(function (arg) {
-    var mainArgument = arg.marg;
-    var supArgument1 = arg.sarg1;
-    var supArgument2 = arg.sarg2;
-    var supArgument3 = arg.sarg3;
-    var opeddate = arg.opd;
-    var newsjournal = arg.njs;
-    var chapter = arg.id3;
-    var termtitle = arg.trm;
-    var termexplanation = arg.texp;
-    var uida = arg.ida;
-    var wnew = arg.nws;
-    var wpoint1 = arg.pnt1;
-    var wpoint2 = arg.pnt2;
-    var wpoint3 = arg.pnt3;
-    var wpoint4 = arg.pnt4;
-    var wpoint5 = arg.pnt5;
-    var wopinionon = arg.opnn;
-    var wopinion1 = arg.opn1;
-    var wopinionby = arg.opnb;
-    var wbooktitle = arg.btle;
-    var wauthortitle = arg.btar;
-    var wpublication = arg.bpne;
-
-    mainArguments.push(mainArgument);
-    supArgument1s.push(supArgument1);
-    supArgument2s.push(supArgument2);
-    supArgument3s.push(supArgument3);
-    opiniondates.push(opeddate);
-    newspaperjournals.push(newsjournal);
-    chapters.push(chapter);
-    termtitles.push(termtitle);
-    termexplanations.push(termexplanation);
-    uidas.push(uida);
-    wnewss.push(wnew);
-    wpoint1s.push(wpoint1);
-    wpoint2s.push(wpoint2);
-    wpoint3s.push(wpoint3);
-    wpoint4s.push(wpoint4);
-    wpoint5s.push(wpoint5);
-    wopinionons.push(wopinionon);
-    wopinion1s.push(wopinion1);
-    wopinionbys.push(wopinionby);
-    wbooktitles.push(wbooktitle);
-    wauthortitles.push(wauthortitle);
-    wpublications.push(wpublication);
-  });
-}).limit(20);
-app.get("/", function (req, res) {
   res.render("index", {
     kindOfDay: day,
     mainArguments: mainArguments,
@@ -196,10 +350,56 @@ app.get("/", function (req, res) {
     wbooktitles: wbooktitles,
     wauthortitles: wauthortitles,
     wpublications: wpublications,
+    urusernames: urusernames,
   });
 });
 
-app.post("/", function (req, res) {
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/study", function (req, res) {
+  var today = new Date();
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("study", {
+    kindOfDay: day,
+    mainArguments: mainArguments,
+    supArgument1s: supArgument1s,
+    supArgument2s: supArgument2s,
+    supArgument3s: supArgument3s,
+    newspaperjournals: newspaperjournals,
+    opiniondates: opiniondates,
+    chapters: chapters,
+    termtitles: termtitles,
+    termexplanations: termexplanations,
+    uidas: uidas,
+    wnewss: wnewss,
+    wpoint1s: wpoint1s,
+    wpoint2s: wpoint2s,
+    wpoint3s: wpoint3s,
+    wpoint4s: wpoint4s,
+    wpoint5s: wpoint5s,
+    wopinionons: wopinionons,
+    wopinion1s: wopinion1s,
+    wopinionbys: wopinionbys,
+    wbooktitles: wbooktitles,
+    wauthortitles: wauthortitles,
+    wpublications: wpublications,
+    urusernames: urusernames,
+    mylistnames: mylistnames,
+  });
+});
+
+app.post("/home", function (req, res) {
   var margName = req.body.mainargument;
   var sarg1Name = req.body.supportingargument1;
   var sarg2Name = req.body.supportingargument2;
@@ -250,7 +450,59 @@ app.post("/", function (req, res) {
 
   arg.save();
 
-  res.redirect("/");
+  res.redirect("/home");
+});
+
+app.post("/study", function (req, res) {
+  var myagreeName = req.body.agreecomment;
+  var myqueryName = req.body.querycomment;
+  var urusernameName = req.body.yourusername;
+  var urenrollidName = req.body.yourenrollid;
+  var mylistnameName = req.body.listname;
+  var myuidargName = req.body.myuida;
+  var anuidargName = req.body.anuida;
+  var uruidargName = req.body.uruida;
+  var myenrollmentnumberName = req.body.myenrollmentnumber;
+  var mylistargnameName = req.body.listargname;
+  var urenrollrefnameName = req.body.urenrollrefname;
+
+  const myessential = new Myessential({
+    myenrollmentnumber: myenrollmentnumberName,
+    mylistname: mylistnameName,
+  });
+
+  const mystudyteam = new Mystudyteam({
+    myenrollmentnumber: myenrollmentnumberName,
+    urusername: urusernameName,
+    urenrollmentnumber: urenrollidName,
+  });
+
+  const mysavedarg = new Mysavedarg({
+    myenrollmentnumber: myenrollmentnumberName,
+    mylistargname: mylistargnameName,
+    myuida: myuidargName,
+  });
+
+  const myreferred = new Myreferred({
+    myenrollmentnumber: myenrollmentnumberName,
+    uruida: uruidargName,
+    urenrollrefname: urenrollrefnameName,
+  });
+
+  const mysocial = new Mysocial({
+    myagreecomment: myagreeName,
+    myquerycomment: myqueryName,
+    anuida: anuidargName,
+    myenrollmentnumber: myenrollmentnumberName,
+  });
+
+  myessential.save();
+  mysavedarg.save();
+  myreferred.save();
+  mystudyteam.save();
+  mysocial.save();
+
+  res.redirect("/study");
 });
 
 let port = process.env.PORT;
